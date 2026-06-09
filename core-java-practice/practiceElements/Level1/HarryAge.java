7e7351844e0f9f5465499d08246c4d49581e9d27
Divyanshu <ds0015103@gmail.com>
2026-06-09 13:44:35 +0530

    [Divyanshu] i have solved the first question

---
+++ core-java-practice/practiceElements/Level1/HarryAge.java
@@ -0,0 +1,10 @@
+package PracticeElements.Level1;
+
+public class HarryAge {
+    public static void main(String[] args) {
+        int currentyear=2024;
+        int birthYear=2000;
+        int age=currentyear-birthYear;
+        System.out.println(age);
+    }
+}
