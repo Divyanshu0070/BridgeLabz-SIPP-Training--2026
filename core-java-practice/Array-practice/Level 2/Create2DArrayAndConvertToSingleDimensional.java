a3ec5ffe21f81db62e0c594097114a9e76192079
Divyanshu <ds0015103@gmail.com>
2026-06-11 21:45:24 +0530

    [Divyanshu].add I have solved problems og array practice level 1&2

---
+++ core-java-practice/Array-practice/Level 2/Create2DArrayAndConvertToSingleDimensional.java
@@ -0,0 +1,47 @@
+package ArrayPractice.Level2;
+
+import java.util.Scanner;
+
+public class Create2DArrayAndConvertToSingleDimensional {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+
+        System.out.print("Enter number of columns: ");
+        int cols = sc.nextInt();
+
+        int[][] matrix = new int[rows][cols];
+
+        // Input 2D array
+        System.out.println("Enter elements of matrix:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        // Create 1D array
+        int[] array = new int[rows * cols];
+        int index = 0;
+
+        // Copy 2D to 1D
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                array[index] = matrix[i][j];
+                index++;
+            }
+        }
+
+        // Display 1D array
+        System.out.println("1D Array:");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+
+        sc.close();
+    }
+}
